46cfb39b972c7d89f056106832e51bd16261bad2
mhmad-nr <83580680+mhmad-nr@users.noreply.github.com>
2024-05-26 16:28:52 +0330

    add swagger doc to project

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { API_PORT } from "./config";
 import { FormatRegistry } from "@sinclair/typebox";
 import { TypeBoxTypeProvider, TypeBoxValidatorCompiler } from "@fastify/type-provider-typebox";
 import { isEmail, isStrongPassword } from "class-validator";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const startServer = async () => {
   try {
@@ -15,10 +17,28 @@ const startServer = async () => {
     FormatRegistry.Set("password", (value) => isStrongPassword(value));
     
     server.withTypeProvider<TypeBoxTypeProvider>();
+
+    // Register Swagger
+    server.register(fastifySwagger, {
+      mode: 'static',
+      specification: {
+
+        path: './swagger.yaml',
+        baseDir: __dirname,
+      },
+    });
+
+    server.register(fastifySwaggerUi, {
+      routePrefix: '/documentation',
+      staticCSP: true,
+      transformSpecification: (swaggerObject, request, reply) => {
+        return swaggerObject;
+      },
+    });
+
     server.register(authRouter, { prefix: "/api/auth" });
     server.register(userRouter, { prefix: "/api/user" });
 
-    
     server.setErrorHandler((error, request, reply) => {
       if (error.validation) {
         reply.status(400).send({
